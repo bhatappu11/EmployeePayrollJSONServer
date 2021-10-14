02b59b41a4c58468268ce507e01277ad22c9c08e
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-14 07:26:55 +0530

    REFACTORED: display employee payroll details from local storage

--- js/home.js
+++ js/home.js
@@ -1,19 +1,29 @@
+let employeePayrollList;
 window.addEventListener('DOMContentLoaded',(event) => {
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ?
+            JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    if(employeePayrollList.length == 0) return;
     let innerHtml = `${headerHtml}`;
-    let empPayrollList = createEmployeePayrollJSON();
-    for(const employeePayrollData of empPayrollList) {
+    //let empPayrollList = createEmployeePayrollJSON();
+    for(const employeePayrollData of employeePayrollList) {
         innerHtml = `${innerHtml}
         <tr>
             <td><img class="profile" alt="" src="${employeePayrollData._profilePic}" alt=""></td>
             <td>${employeePayrollData._name}</td>
             <td>${employeePayrollData._gender}</td>
-            <td>${getDeptHtml(employeePayrollData._department)}</td>
+            <td>${getDeptHtml(employeePayrollData._departments)}</td>
             <td>${employeePayrollData._salary}</td>
             <td>${employeePayrollData._startDate}</td>
             <td>
@@ -34,34 +44,4 @@ const getDeptHtml = (deptList) => {
         deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
     }
     return deptHtml;
-}
-const createEmployeePayrollJSON = () => {
-    let empPayrollListLocal = [
-        {
-            _name: 'Sheshadri Bhat',
-            _gender: 'Male',
-            _department: [
-                'HR',
-                'Finance'
-            ],
-            _salary: '500000',
-            _startDate: '11 Oct 2021',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/profile-images/Ellipse -2.png'
-        },
-        {
-            _name: 'Smitha Naveen',
-            _gender: 'Female',
-            _department: [
-                'Finance'
-            ],
-            _salary: '400000',
-            _startDate: '3 Oct 2021',
-            _note: '',
-            _id: new Date().getTime() + 1,
-            _profilePic: '../assets/profile-images/Ellipse -4.png'
-        }
-    ];
-    return empPayrollListLocal;
 }
