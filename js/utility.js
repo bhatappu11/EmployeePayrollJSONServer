1b1de01c03f660406b180fc6a027eef7f583b0db
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-19 05:02:14 +0530

    MODIFIED: update method is shifted to employee payroll service js

--- js/utility.js
+++ js/utility.js
@@ -5,13 +5,6 @@ const stringifyDate = (date) => {
     return newDate;
 }
 
-const update = (node) => {
-    let employeePayrollData = employeePayrollList.find(empData => empData._id == node.id);
-    if(!employeePayrollData) return;
-    localStorage.setItem('editEmp',JSON.stringify(employeePayrollData))
-    window.location.replace(site_properties.add_emp_payroll_page);
-}
-
 const checkName = (name) => {
     let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
     if(!nameRegex.test(name)) throw 'Name is Incorrect!';
