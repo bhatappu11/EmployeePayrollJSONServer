08ac129cdfc62fdb00fc07f2f01b8a001fe3b8a0
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-18 22:28:23 +0530

    ADDED: method to validate name and start date

--- js/utility.js
+++ js/utility.js
@@ -11,3 +11,16 @@ const update = (node) => {
     localStorage.setItem('editEmp',JSON.stringify(employeePayrollData))
     window.location.replace(site_properties.add_emp_payroll_page);
 }
+
+const checkName = (name) => {
+    let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(!nameRegex.test(name)) throw 'Name is Incorrect!';
+}
+
+const checkStartDate = (startDate) => {
+    if(startDate >= new Date())
+        throw 'Start Date can not be a future date';
+    let days = Math.abs(startDate - new Date())/(1000*60*60*24);
+    if(days > 30)
+        throw 'Start Date is beyond 30 days';
+}
